c596f3451a021e0c1b7ff1dc6a739709a2695d4a
Nikita Karaban <karabann@rocketdata.io>
2024-03-24 12:52:50 +0300

    FRONT-010: fix build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,7 @@ import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import {type BuildOptions} from './types/config';
 
 export function buildPlugins({paths: path, isDev}: BuildOptions): Array<webpack.WebpackPluginInstance> {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: path.html,
         }),
@@ -21,9 +21,16 @@ export function buildPlugins({paths: path, isDev}: BuildOptions): Array<webpack.
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
+        );
+    }
+
+    return plugins;
 }
